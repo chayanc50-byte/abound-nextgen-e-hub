fff12598ea6986678d524d66a35485bb68340a86
Chayan <chayan@Chayans-Mac-mini-2.local>
2026-07-20 12:48:20 +0530

    correct icon for app

--- hierarchy.py
+++ hierarchy.py
@@ -8,7 +8,7 @@ LEVELS = [
     (4, "Super Franchise"),
     (5, "Merchant"),
     (6, "Super Merchant"),
-    (7, "Core Merchant Team"),
+    (7, "Core Team of Merchants"),
     (8, "Business Development Director"),
     (9, "Senior Director"),
     (10, "Chief Business Development Director"),
@@ -16,16 +16,16 @@ LEVELS = [
 
 # Commission % by level (1-10)
 COMMISSION_BY_LEVEL = {
-    1: 10.0,
-    2: 4.0,
-    3: 3.0,
-    4: 2.0,
-    5: 1.5,
-    6: 1.0,
-    7: 1.0,
-    8: 1.0,
-    9: 1.0,
-    10: 0.5,
+    1: 20.0,
+    2: 8.0,
+    3: 6.0,
+    4: 4.0,
+    5: 3.0,
+    6: 2.0,
+    7: 2.0,
+    8: 2.0,
+    9: 2.0,
+    10: 1.0,
 }
 
 # Promotion: need 10 direct referrals at your level to reach next level
